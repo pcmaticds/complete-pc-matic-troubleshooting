3c095c6a275c5acb3f5c6a7ade5abf57787ae544
pcmaticds <kjhgdhu@outlook.com>
2026-07-11 12:58:55 +0530

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,20 @@
+project = 'Complete-Pc-Matic-Troubleshooting-Guide'
+author = 'Complete-Pc-Matic-Troubleshooting-Guide'
+release = '1.0'
+
+# Extensions
+extensions = [
+    'sphinx_sitemap',
+]
+
+# Paths
+templates_path = ['_templates']
+exclude_patterns = []
+
+# Theme
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+# Custom JS & Favicon
+html_js_files = ['chatbot.js']  # chatbot widget
+html_favicon = '_static/favicon.png'
